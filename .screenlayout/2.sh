f1f2ec1f3be4a42a9e14f1b31d43ef0946ca47ae
Noah Torrance <noahtorrance27@gmail.com>
2023-02-08 09:24:43 +0000

    Remove arandr config

--- .screenlayout/2.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output eDP-1 --primary --mode 1920x1080 --pos 1600x0 --rotate normal --output HDMI-1 --off --output DP-1 --off --output HDMI-1-0 --mode 1600x900 --pos 0x90 --rotate normal
